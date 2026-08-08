b69772e7fbf6a5d75569e53885a5d858f1bf9ceb
Inna Putri Meida <innaputrimeida@gmail.com>
2026-08-08 11:27:02 +0700

    Add D:\Wisuda Teh Vivi to scanned local directories

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 const PORT = 3000;
 
 // Folder lokal yang di-scan secara rekursif
-const LOCAL_DIRS = ['D:\\FOTO', 'D:\\HP', 'D:\\Keluarga Besar Cirea'];
+const LOCAL_DIRS = ['D:\\FOTO', 'D:\\HP', 'D:\\Keluarga Besar Cirea', 'D:\\Wisuda Teh Vivi'];
 const ACCOUNTS_FILE = path.join(__dirname, 'accounts.json');
 
 app.use(express.json());
